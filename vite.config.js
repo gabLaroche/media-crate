b52e5553f171f0153c014186bef53b0bb42b9684
Gabriel Laroche <gabriel.laroche@protonmail.com>
2026-05-08 17:20:13 -0400

    Switch PWA register type to autoUpdate
    Prevents users from having to reinstall the PWA to get updates.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       workbox: {
         runtimeCaching: [
           {
